c0892ad7f805edaef46ce0d85399e79c18c3722c
Ferhan Abacı <84642053+ferhanabaci@users.noreply.github.com>
2022-06-30 22:27:36 +0300

    denemeler yapıldı

---
+++ TravelTripProje3/Migrations/202206301855480_mig_iletisim.cs
@@ -0,0 +1,22 @@
+﻿namespace TravelTripProje3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig_iletisim : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.iletisims", "Telefon", c => c.String());
+            AddColumn("dbo.iletisims", "Sehir", c => c.String());
+            DropColumn("dbo.iletisims", "Konu");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.iletisims", "Konu", c => c.String());
+            DropColumn("dbo.iletisims", "Sehir");
+            DropColumn("dbo.iletisims", "Telefon");
+        }
+    }
+}
